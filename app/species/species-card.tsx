ad5745ab88b4dbb45c59aae84c5caf1c6ec6ebc5
Jessica Li <jessicali1703@gmail.com>
2023-09-17 01:28:19 -0400

    additional info

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
+import { useState } from "react";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard(species: Species) {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -14,8 +24,27 @@ export default function SpeciesCard(species: Species) {
       <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
       <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
-      {/* Replace with detailed view */}
-      <Button className="mt-3 w-full">Learn More</Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="secondary" className="mt-3 w-full" onClick={() => setOpen(true)}>
+            Learn More
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>{species.common_name}</DialogTitle>
+            <DialogDescription>
+              <div className="flex flex-col">
+                <div>{species.scientific_name}</div>
+                <div>{species.image}</div>
+                <div>{species.total_population}</div>
+                <div>{species.kingdom}</div>
+                <div>{species.description}</div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
