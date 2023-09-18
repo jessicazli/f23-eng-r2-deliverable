17bf5a5721f959153ab1190b3f06445efd0462ff
Jessica Li <jessicali1703@gmail.com>
2023-09-17 21:30:30 -0400

    prettier

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -15,23 +15,25 @@ import Image from "next/image";
 import { useState } from "react";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
-export default function SpeciesCard({ species, userId }: { species: Species, userId: string }) {
+export default function SpeciesCard({ species, userId }: { species: Species; userId: string }) {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow flex flex-col">
-      {userId === species.author ?
+    <div className="min-w-72 m-4 flex w-72 flex-none flex-col rounded border-2 p-3 shadow">
+      {userId === species.author ? (
         <div className="flex items-center justify-between">
           <h3 className="text-2xl font-semibold">{species.common_name}</h3>
           <div className="flex justify-end gap-2 text-gray-500">
-            <Icons.pencil className="h-5 w-5 hover:text-black"/>
+            <Icons.pencil className="h-5 w-5 hover:text-black" />
             <Icons.trash className="h-5 w-5 hover:text-red-600" />
           </div>
         </div>
-        : <h3 className="text-2xl font-semibold">{species.common_name}</h3>}
+      ) : (
+        <h3 className="text-2xl font-semibold">{species.common_name}</h3>
+      )}
       <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
       {species.image && (
-        <div className="relative h-40 w-full my-4">
+        <div className="relative my-4 h-40 w-full">
           <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
         </div>
       )}
@@ -47,11 +49,13 @@ export default function SpeciesCard({ species, userId }: { species: Species, use
             <DialogHeader>
               <DialogTitle className="text-2xl font-semibold">{species.common_name}</DialogTitle>
               <DialogDescription>
-                <div className="text-lg font-light italic pb-4">{species.scientific_name}</div>
+                <div className="pb-4 text-lg font-light italic">{species.scientific_name}</div>
                 <div className="text-lg">Kingdom: {species.kingdom}</div>
-                {species.total_population && <div className="text-lg">Total Population: {species.total_population}</div>}
+                {species.total_population && (
+                  <div className="text-lg">Total Population: {species.total_population}</div>
+                )}
                 {species.image && (
-                  <div className="relative h-80 w-full my-4">
+                  <div className="relative my-4 h-80 w-full">
                     <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
                   </div>
                 )}
